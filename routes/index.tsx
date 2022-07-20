0d5bc191048e20bc0b307c483c3d36c570ff6b9c
Syed Umair <syed.yousuff@anywhere.co>
2022-07-20 18:18:12 +0530

    refactored project

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,21 +1,34 @@
 /** @jsx h */
 import { h } from "preact";
 import { tw } from "@twind";
-import Counter from "../islands/Counter.tsx";
 
 export default function Home() {
-  return (
-    <div class={tw`p-4 mx-auto max-w-screen-md`}>
-      <img
-        src="/logo.svg"
-        height="100px"
-        alt="the fresh logo: a sliced lemon dripping with juice"
-      />
-      <p class={tw`my-6`}>
-        Welcome to `fresh`. Try update this message in the ./routes/index.tsx
-        file, and refresh.
-      </p>
-      <Counter start={3} />
-    </div>
-  );
+	return (
+		<div class={tw`flex flex-col h-screen justify-center items-center`}>
+			<figure class={tw`bg-slate-100 rounded-xl p-8 dark:bg-slate-800`}>
+				<img
+					class={tw`w-72 h-72 rounded-full mx-auto`}
+					src="/Syed.jpeg"
+					alt="Syed Umair"
+				/>
+				<div class={tw`pt-6 text-center space-y-4`}>
+					<blockquote>
+						<p class={tw`text-lg font-medium`}>
+							“Success is being an integral part of building
+							products, without which my future kids couldn't
+							imagine living”
+						</p>
+					</blockquote>
+					<figcaption class={tw`font-medium`}>
+						<div class={tw`text-sky-500 dark:text-sky-400`}>
+							Syed Umair
+						</div>
+						<div class={tw`text-slate-700 dark:text-slate-500`}>
+							Module Lead, FULL Creative
+						</div>
+					</figcaption>
+				</div>
+			</figure>
+		</div>
+	);
 }
